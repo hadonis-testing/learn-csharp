14520380a2021e3ebe7d84e4468c4faeff60e7c1
Minh Vương <hardingadonis@gmail.com>
2024-05-10 21:36:07 +0700

    update: done CS01

---
+++ CS01/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Day_1
+{
+    internal class Program
+    {
+        /// <summary>
+        /// The entry point
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            string name = "Minh Vương";
+
+            Console.Title = "CS01 " + name;
+
+            Console.WriteLine(name);
+
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
