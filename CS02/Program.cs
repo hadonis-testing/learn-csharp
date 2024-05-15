5c92b3768c5109df25f3ba6a4485910c7c7d60cd
Minh Vương <hardingadonis@gmail.com>
2024-05-16 01:31:27 +0700

    update: upload CS02

---
+++ CS02/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace CS02
+{
+    internal class Program
+    {
+        /// <summary>
+        /// The entry point
+        /// </summary>
+        /// <param name="args">Arguments</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
