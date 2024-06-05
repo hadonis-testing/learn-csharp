8dfae1c3fbf7705c15913e021a897feb2056a29f
Minh Vương <hardingadonis@gmail.com>
2024-06-05 14:21:57 +0700

    [Add] Upload CS06

---
+++ CS06/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace CS06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+
+            try
+            {
+                Console.WriteLine(5 / a);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
