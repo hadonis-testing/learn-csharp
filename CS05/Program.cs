cc115e0b7eb37fa08caf286c3a1ace579692809a
Minh Vương <hardingadonis@gmail.com>
2024-06-05 10:17:59 +0700

    [Add] Upload CS05

---
+++ CS05/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+using System.Runtime.CompilerServices;
+
+namespace CS05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Extension Method";
+
+            str.PrintWithColor(ConsoleColor.Green);
+
+            "Xin chao moi nguoi".PrintWithColor(ConsoleColor.Blue);
+        }
+    }
+}
