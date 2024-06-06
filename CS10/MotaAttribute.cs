173e05deda8842429f2f46652db4d6a504640d29
Minh Vương <hardingadonis@gmail.com>
2024-06-06 13:32:30 +0700

    [Add] Upload CS10

---
+++ CS10/MotaAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS10
+{
+    [AttributeUsage(AttributeTargets.All)]
+    public class MotaAttribute : Attribute
+    {
+        public string ThongTinChiTiet { get; set; }
+
+        public MotaAttribute(string _ThongTinChiTiet)
+        {
+            ThongTinChiTiet = _ThongTinChiTiet;
+        }
+    }
+}
---
+++ CS10/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CS10
+{
+    [Mota("Chương trình chính")]
+    internal class Program
+    {
+        [Mota("Hàm main")]
+        static void Main(string[] args)
+        {
+            string a = "Hello World";
+
+            var t = a.GetType();
+
+            Console.WriteLine(t.FullName);
+            t.GetMethods().ToList().ForEach(p => Console.WriteLine(p.Name));
+        }
+    }
+}
