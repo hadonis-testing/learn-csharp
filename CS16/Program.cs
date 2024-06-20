9e1200e03d330b024db47b090de30dfbcb7cb525
Minh Vương <hardingadonis@gmail.com>
2024-06-20 13:50:05 +0700

    [Add] Upload CS16

---
+++ CS16/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CS16
+{
+    internal class Program
+    {
+        public static IEnumerable<int> GetNumbers()
+        {
+            yield return 1;
+            yield return 2;
+            yield return 3;
+        }
+
+        static void Main(string[] args)
+        {
+            foreach (int i in GetNumbers()) Console.WriteLine(i);
+        }
+    }
+}
